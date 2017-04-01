fc36718ba69bc402365901a0346d530e310d5a49
Robert Gies <mail@rgies.de>
2017-04-01 15:01:07 +0200

    Add dashboard entity and improve form generation

---
+++ src/Rgies/MetricsBundle/Controller/DashboardController.php
@@ -0,0 +1,249 @@
+<?php
+
+namespace Rgies\MetricsBundle\Controller;
+
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
+use Rgies\MetricsBundle\Entity\Dashboard;
+use Rgies\MetricsBundle\Form\DashboardType;
+
+/**
+ * Dashboard controller.
+ *
+ * @Route("/dashboard")
+ */
+class DashboardController extends Controller
+{
+
+    /**
+     * Lists all Dashboard entities.
+     *
+     * @Route("/", name="dashboard")
+     * @Method("GET")
+     * @Template()
+     */
+    public function indexAction()
+    {
+        $em = $this->getDoctrine()->getManager();
+
+        $entities = $em->getRepository('MetricsBundle:Dashboard')->findAll();
+
+        return array(
+            'entities' => $entities,
+        );
+    }
+    /**
+     * Creates a new Dashboard entity.
+     *
+     * @Route("/", name="dashboard_create")
+     * @Method("POST")
+     * @Template("MetricsBundle:Dashboard:new.html.twig")
+     */
+    public function createAction(Request $request)
+    {
+        $entity = new Dashboard();
+        $form = $this->createCreateForm($entity);
+        $form->handleRequest($request);
+
+        if ($form->isValid()) {
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($entity);
+            $em->flush();
+
+            return $this->redirect($this->generateUrl('dashboard'));
+        }
+
+        return array(
+            'entity' => $entity,
+            'form'   => $form->createView(),
+        );
+    }
+
+    /**
+     * Creates a form to create a Dashboard entity.
+     *
+     * @param Dashboard $entity The entity
+     *
+     * @return \Symfony\Component\Form\Form The form
+     */
+    private function createCreateForm(Dashboard $entity)
+    {
+        $form = $this->createForm(new DashboardType(), $entity, array(
+            'action' => $this->generateUrl('dashboard_create'),
+            'method' => 'POST',
+            'attr'   => array('id' => 'create-form'),
+        ));
+
+        //$form->add('submit', 'submit', array('label' => 'Create'));
+
+        return $form;
+    }
+
+    /**
+     * Displays a form to create a new Dashboard entity.
+     *
+     * @Route("/new", name="dashboard_new")
+     * @Method("GET")
+     * @Template()
+     */
+    public function newAction()
+    {
+        $entity = new Dashboard();
+        $form   = $this->createCreateForm($entity);
+
+        return array(
+            'entity' => $entity,
+            'form'   => $form->createView(),
+        );
+    }
+
+    /**
+     * Finds and displays a Dashboard entity.
+     *
+     * @Route("/{id}", name="dashboard_show")
+     * @Method("GET")
+     * @Template()
+     */
+    public function showAction($id)
+    {
+        $em = $this->getDoctrine()->getManager();
+
+        $entity = $em->getRepository('MetricsBundle:Dashboard')->find($id);
+
+        if (!$entity) {
+            throw $this->createNotFoundException('Unable to find Dashboard entity.');
+        }
+
+        $deleteForm = $this->createDeleteForm($id);
+
+        return array(
+            'entity'      => $entity,
+            'delete_form' => $deleteForm->createView(),
+        );
+    }
+
+    /**
+     * Displays a form to edit an existing Dashboard entity.
+     *
+     * @Route("/{id}/edit", name="dashboard_edit")
+     * @Method("GET")
+     * @Template()
+     */
+    public function editAction($id)
+    {
+        $em = $this->getDoctrine()->getManager();
+
+        $entity = $em->getRepository('MetricsBundle:Dashboard')->find($id);
+
+        if (!$entity) {
+            throw $this->createNotFoundException('Unable to find Dashboard entity.');
+        }
+
+        $editForm = $this->createEditForm($entity);
+        $deleteForm = $this->createDeleteForm($id);
+
+        return array(
+            'entity'      => $entity,
+            'edit_form'   => $editForm->createView(),
+            'delete_form' => $deleteForm->createView(),
+        );
+    }
+
+    /**
+    * Creates a form to edit a Dashboard entity.
+    *
+    * @param Dashboard $entity The entity
+    *
+    * @return \Symfony\Component\Form\Form The form
+    */
+    private function createEditForm(Dashboard $entity)
+    {
+        $form = $this->createForm(new DashboardType(), $entity, array(
+            'action' => $this->generateUrl('dashboard_update', array('id' => $entity->getId())),
+            'method' => 'PUT',
+            'attr'   => array('id' => 'edit-form'),
+        ));
+
+        //$form->add('submit', 'submit', array('label' => 'Update'));
+
+        return $form;
+    }
+    /**
+     * Edits an existing Dashboard entity.
+     *
+     * @Route("/{id}", name="dashboard_update")
+     * @Method("PUT")
+     * @Template("MetricsBundle:Dashboard:edit.html.twig")
+     */
+    public function updateAction(Request $request, $id)
+    {
+        $em = $this->getDoctrine()->getManager();
+
+        $entity = $em->getRepository('MetricsBundle:Dashboard')->find($id);
+
+        if (!$entity) {
+            throw $this->createNotFoundException('Unable to find Dashboard entity.');
+        }
+
+        $deleteForm = $this->createDeleteForm($id);
+        $editForm = $this->createEditForm($entity);
+        $editForm->handleRequest($request);
+
+        if ($editForm->isValid()) {
+            $em->flush();
+
+            return $this->redirect($this->generateUrl('dashboard'));
+        }
+
+        return array(
+            'entity'      => $entity,
+            'edit_form'   => $editForm->createView(),
+            'delete_form' => $deleteForm->createView(),
+        );
+    }
+    /**
+     * Deletes a Dashboard entity.
+     *
+     * @Route("/{id}", name="dashboard_delete")
+     * @Method("DELETE")
+     */
+    public function deleteAction(Request $request, $id)
+    {
+        $form = $this->createDeleteForm($id);
+        $form->handleRequest($request);
+
+        if ($form->isValid()) {
+            $em = $this->getDoctrine()->getManager();
+            $entity = $em->getRepository('MetricsBundle:Dashboard')->find($id);
+
+            if (!$entity) {
+                throw $this->createNotFoundException('Unable to find Dashboard entity.');
+            }
+
+            $em->remove($entity);
+            $em->flush();
+        }
+
+        return $this->redirect($this->generateUrl('dashboard'));
+    }
+
+    /**
+     * Creates a form to delete a Dashboard entity by id.
+     *
+     * @param mixed $id The entity id
+     *
+     * @return \Symfony\Component\Form\Form The form
+     */
+    private function createDeleteForm($id)
+    {
+        return $this->createFormBuilder(null, array('attr'=>array('id' => 'delete-form')))
+            ->setAction($this->generateUrl('dashboard_delete', array('id' => $id)))
+            ->setMethod('DELETE')
+            //->add('submit', 'submit', array('label' => 'Delete'))
+            ->getForm()
+        ;
+    }
+}
---
+++ src/Rgies/MetricsBundle/Entity/Dashboard.php
@@ -0,0 +1,97 @@
+<?php
+
+namespace Rgies\MetricsBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Dashboard
+ *
+ * @ORM\Table()
+ * @ORM\Entity(repositoryClass="Rgies\MetricsBundle\Entity\DashboardRepository")
+ */
+class Dashboard
+{
+    /**
+     * @var integer
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="title", type="string", length=100)
+     */
+    private $title;
+
+    /**
+     * @var integer
+     *
+     * @ORM\Column(name="order", type="integer")
+     */
+    private $order;
+
+
+    /**
+     * Get id
+     *
+     * @return integer
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set title
+     *
+     * @param string $title
+     *
+     * @return Dashboard
+     */
+    public function setTitle($title)
+    {
+        $this->title = $title;
+
+        return $this;
+    }
+
+    /**
+     * Get title
+     *
+     * @return string
+     */
+    public function getTitle()
+    {
+        return $this->title;
+    }
+
+    /**
+     * Set order
+     *
+     * @param integer $order
+     *
+     * @return Dashboard
+     */
+    public function setOrder($order)
+    {
+        $this->order = $order;
+
+        return $this;
+    }
+
+    /**
+     * Get order
+     *
+     * @return integer
+     */
+    public function getOrder()
+    {
+        return $this->order;
+    }
+}
+
